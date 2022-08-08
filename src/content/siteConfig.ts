a89266abf5fccd80c9574a2fe52d3e6a896edd05
Mitch Stewart <mitchell.stewart08@gmail.com>
2022-08-08 13:50:20 -0500

    remove footer links

--- src/content/siteConfig.ts
+++ src/content/siteConfig.ts
@@ -4,10 +4,7 @@ export const siteConfig = {
   favicon: '/avatar.svg',
   ogImage: '/fb-social-share.jpg',
   url: 'https://mitchstewart.codes',
-  // footerLinks: [
-  //   { title: 'Link 1', url: '/link-1' },
-  //   { title: 'Link 2', url: '/link-2' },
-  //   { title: 'Link 3', url: '/link-3' },
-  //   { title: 'Link 4', url: '/link-4' },
-  // ]
+  footerLinks: [
+    // { title: 'Link 1', url: '/link-1' },
+  ]
 }
